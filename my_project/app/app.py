e2cb774468a2216ec3a44bfd4466c4e509bdb60d
solnyszczko <130127637+solnyszczko@users.noreply.github.com>
2023-05-17 21:26:00 -0700

    Add files via upload

--- my_project/app/app.py
+++ my_project/app/app.py
@@ -1,8 +1,7 @@
-from beanie import init_beanie
+
 from fastapi import Depends, FastAPI, HTTPException, Request, Response, status, WebSocket, WebSocketDisconnect
 
-from rich import inspect, print
-from rich.console import Console
+
 
 from fastapi.responses import HTMLResponse, RedirectResponse
 from typing import Dict, List, Optional
@@ -11,70 +10,57 @@ import random
 import uuid
 import json
 import redis
+#import redis.asyncio as redis
+import datetime
 
-from app.db import User, db
-from app.schemas import UserCreate, UserRead, UserUpdate
-from app.users import (
-    SECRET,
-    auth_backend,
-    current_active_user,
-    fastapi_users,
-    google_oauth_client,
-)
+from pydantic import EmailStr
+
+from redis_om import HashModel
+
+
+class Player(HashModel):
+    name: str
+    x: int
+    y: int
+    z: int
+    id:str
+ #   time_created: datetime.date
+ #   bio: Optional[str]
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-console = Console()
+
 
 pool = redis.ConnectionPool(host='localhost', port=6379, db=0, decode_responses=True)
-redis = redis.Redis(connection_pool=pool)
+red = redis.StrictRedis(connection_pool=pool)
 
-redis.set('mykey', 'Hello from Python!')
-value = redis.get('mykey')
-print(value)
-redis.flushdb()
+red.flushdb()
 
-
-class Player:
-    def __init__(self, name: str, x: int, y: int, z: int, id:str):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.z = z
-        self.id = id
-
-# Set up the game state
-game_state = {
-    'players': {},
-    'objects': {},
-    'score': 0,
-    # ... etc.
-}
 
 active_players = dict()
 
 def input_handler(data,id):
     if data == "up":
-        redis.hincrby(str(id), 'y',-1 ) 
+        red.hincrby(str(id), 'y',-1 ) 
     if data == "down":
-        redis.hincrby(str(id), 'y',1 ) 
+        red.hincrby(str(id), 'y',1 ) 
     if data == "left":
-        redis.hincrby(str(id), 'x',-1 ) 
+        red.hincrby(str(id), 'x',-1 ) 
     if data == "right":
-        redis.hincrby(str(id), 'x',1 )                       
+        red.hincrby(str(id), 'x',1 )                       
 
 def generate_character(id):
     return Player(name = ("meow"+ id), x = random.randint(1,10), y = random.randint(1,10), z = random.randint(1,10), id = id)
 
 def redis_gen_state():
-    keys = redis.keys('*')
+    keys = red.keys('*')
     values = []
     # Retrieve values for each key
     for key in keys:
         print(key)
-        value = redis.hgetall(key)  # or r.hget(hash_key, field) for hash data structure
+        value = red.hgetall(key)  # or r.hget(hash_key, field) for hash data structure
         values.append(value)
     print("MEOW")
     print(values)
@@ -91,7 +77,7 @@ class ConnectionManager:
         websocket.id = uuid.uuid4()
         
        # active_players[str(websocket.id)] = generate_character(str(websocket.id)).__dict__
-        redis.hmset(str(websocket.id), generate_character(str(websocket.id)).__dict__)
+        red.hmset(str(websocket.id), generate_character(str(websocket.id)).__dict__)
      #   print(active_players)
      #   print(websocket.id)
       #  print(websocket.headers)
@@ -101,7 +87,7 @@ class ConnectionManager:
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
-        redis.delete(str(websocket.id))
+        red.delete(str(websocket.id))
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -134,14 +120,14 @@ async def get():
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     await manager.broadcast_game_state(redis_gen_state())
+    #BROADCAST INITIAL DATA
     print("desu")
-    try:
+    try: #CLIENT UPDATE LOOP
         while True:
             data = await websocket.receive_text()
             input_handler(data,websocket.id)
             await manager.broadcast_game_state(redis_gen_state())
-     #       await manager.send_personal_message(f"You wrote: {data}", websocket)
-      #      await manager.broadcast(f"Client #{client_id} says: {data}")
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast_game_state(redis_gen_state())
